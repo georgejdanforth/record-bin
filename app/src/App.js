847d9c086782facc161c53d4767ad9f89ad9e084
George J. Danforth <georgejdanforth@gmail.com>
2018-11-22 10:21:51 -0500

    eslint config

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
             Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
