d0a4d16028cc242835ffa94db04248b04b3fcae1
George J. Danforth <georgejdanforth@gmail.com>
2018-12-24 12:09:25 -0500

    fixed spinner centering for cross site track adding

--- app/src/components/LoadingOverlay.js
+++ app/src/components/LoadingOverlay.js
@@ -5,11 +5,13 @@ import { SpinnerIcon } from './icons';
 
 const LoadingOverlay = () => (
     <div className="loading-overlay">
-        <SpinnerIcon
-            size={5}
-            color={'white'}
-            spin={1}
-        />
+        <div className="loading-spinner">
+            <SpinnerIcon
+                size={5}
+                color={'white'}
+                spin={1}
+            />
+        </div>
     </div>
 );
 
